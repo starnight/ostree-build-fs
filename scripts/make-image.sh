8da7b8375a4dd6c9f0ac99195eff5c482158e646
Jian-Hong Pan <chienhung.pan@gmail.com>
2025-04-19 18:43:56 +0800

    make-image.sh: Avoid mount modloop when boot
    Avoid mount modloop when boot by passing modloop=none to kernel's boot
    command.

--- scripts/make-image.sh
+++ scripts/make-image.sh
@@ -40,7 +40,7 @@ ostree admin init-fs "${TARGET_DIR}"
 ostree admin --sysroot="${TARGET_DIR}" os-init ${OSNAME}
 ostree --repo="${REPOPATH}" remote add ${OSTREE_REMOTE_NAME} ${OSTREE_SERVER_URL} ${OSTREE_BRANCH} --no-gpg-verify
 ostree --repo="${REPOPATH}" pull ${OSTREE_REMOTE_NAME} ${OSTREE_BRANCH}
-ostree admin deploy --sysroot="${TARGET_DIR}" --os=${OSNAME} --karg=rw --karg=console=ttyS0 ${OSTREE_REMOTE_NAME}:${OSTREE_BRANCH}
+ostree admin deploy --sysroot="${TARGET_DIR}" --os=${OSNAME} --karg=modloop=none --karg=rw --karg=console=ttyS0 ${OSTREE_REMOTE_NAME}:${OSTREE_BRANCH}
 OSTREE_CURRENT_DEPLOYMENT=$(ostree admin --sysroot="${TARGET_DIR}" status | awk '{print $2; exit}')
 
 echo "Prepare Boot Partitionn"
